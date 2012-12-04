c68c202bed2489993312286f0d5dda87575f6bce
Sigel <entireboy@daum.net>
2012-12-04 19:59:11 +0900

    테스트 MongoDB 데이터 수정 1. index 추가

---
+++ test/initData/index.js
@@ -0,0 +1,52 @@
+var mongodb = require('mongodb');
+var CONFIG = require('config');
+
+
+var datas = [
+  {collection: 'file', index: {'user.own': 1}}
+  , {collection: 'file', index: {'user.views': 1}}
+  , {collection: 'file', index: {'user.edits': 1}}
+];
+
+
+
+// MongoDB connection
+var mongoServer = new mongodb.Server(
+  CONFIG.mongodb.host
+  , CONFIG.mongodb.port
+  , {
+    auto_reconnect:CONFIG.mongodb.autoReconnect
+    , poolSize:CONFIG.mongodb.pollSize
+});
+
+var db = new mongodb.Db(CONFIG.mongodb.database, mongoServer,{safe:true});
+
+function ensureIndex(collection, index) {
+  db.collection(collection, function(err, collection) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+
+    collection.ensureIndex(index, function(err) {
+      if(err) {
+        console.log(err);
+        return;
+      }
+      checkFin();
+    });
+  });
+}
+
+var total = datas.length;
+var count = 0;
+function checkFin() {
+  count++;
+  if(total == count) {
+    db.close();
+  }
+}
+
+datas.forEach(function(item, i) {
+  ensureIndex(item.collection, item.index);
+});
--- test/initData/saveTestData.js
+++ test/initData/saveTestData.js
@@ -3,39 +3,36 @@ var CONFIG = require('config');
 
 
 module.exports.save = function(collection, key, datas) {
-  
-
-
-// MongoDB connection
-var mongoServer = new mongodb.Server(
-  CONFIG.mongodb.host
-  , CONFIG.mongodb.port
-  , {
-    auto_reconnect:CONFIG.mongodb.autoReconnect
-    , poolSize:CONFIG.mongodb.pollSize
-});
-
-var db = new mongodb.Db(CONFIG.mongodb.database, mongoServer,{safe:true});
-
-db.collection(collection, function(err, collection) {
-  if(err) {
-    console.log(err);
-    return;
-  }
-  
-  datas.forEach(function(data, i) {
-    var criteria = {};
-    criteria[key] = data[key];
-    collection.update(criteria, data, {upsert: true}, function(err, obj) { checkFin(); });
+  // MongoDB connection
+  var mongoServer = new mongodb.Server(
+    CONFIG.mongodb.host
+    , CONFIG.mongodb.port
+    , {
+      auto_reconnect:CONFIG.mongodb.autoReconnect
+      , poolSize:CONFIG.mongodb.pollSize
   });
-});
 
-var total = datas.length;
-var count = 0;
-function checkFin() {
-  count++;
-  if(total == count) {
-    db.close();
+  var db = new mongodb.Db(CONFIG.mongodb.database, mongoServer,{safe:true});
+
+  db.collection(collection, function(err, collection) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+    
+    datas.forEach(function(data, i) {
+      var criteria = {};
+      criteria[key] = data[key];
+      collection.update(criteria, data, {upsert: true}, function(err, obj) { checkFin(); });
+    });
+  });
+
+  var total = datas.length;
+  var count = 0;
+  function checkFin() {
+    count++;
+    if(total == count) {
+      db.close();
+    }
   }
-}
 };
