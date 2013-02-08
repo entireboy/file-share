d0be84b7b7c4da28a368b66e3634c0271b024f8c
Sigel <entireboy@daum.net>
2013-02-08 11:25:21 +0900

    사용자 파일 목록 로딩 Infinite Scroll Pagination에 timeout 추가

--- test/initData/saveTestFile.js
+++ test/initData/saveTestFile.js
@@ -5,6 +5,7 @@ var files = [
   , {_id:'f2', file:{name:'test.mp4', path:'/u1/test.mp4', time:new Date()}, user:{own:'u1'}}
   , {_id:'f3', file:{name:'test.txt', path:'/u2/test.txt', time:new Date()}, user:{own:'u2', views:['u1']}}
   , {_id:'f4', file:{name:'test.xlsx', path:'/u2/test.xlsx', time:new Date()}, user:{own:'u2'}}
+  , {_id:'f5', file:{name:'test.png', path:'/u1/test.png', time:new Date()}, user:{own:'u1', edits:['u2']}}
 ];
 
 saver.save('file', '_id', files);
