f3434bcd72e5e4dbfa872f0115f9363cab7ebe84
Sigel <entireboy@daum.net>
2013-04-26 22:11:29 +0900

    테스트 파일 생성 시간 조절  - pagination을 위해 동일한 시간이 존재하면 안 됨  - sleep이 없어 간단하게 시간 조절로 사용

--- test/initData/saveTestFile.js
+++ test/initData/saveTestFile.js
@@ -1,11 +1,11 @@
 var saver = require('./saveTestData.js');
 
 var files = [
-  {_id:'11111', file:{name:'test.jpg', path:'/u1/test.jpg', time:new Date()}, share:'PRIVATE', user:{own:'u1', edits:['u2']}}
-  , {_id:'22222', file:{name:'test.mp4', path:'/u1/test.mp4', time:new Date()}, share:'PRIVATE', user:{own:'u1'}}
-  , {_id:'33333', file:{name:'test.txt', path:'/u2/test.txt', time:new Date()}, share:'PRIVATE', user:{own:'u2', views:['u1']}}
-  , {_id:'44444', file:{name:'test.xlsx', path:'/u2/test.xlsx', time:new Date()}, share:'PRIVATE', user:{own:'u2'}}
-  , {_id:'55555', file:{name:'test.png', path:'/u1/test.png', time:new Date()}, share:'PUBLIC', user:{own:'u1', edits:['u2']}}
+  {_id:'11111', file:{name:'test.jpg', path:'/u1/test.jpg', time:new Date(Date.now() + 10)}, share:'PRIVATE', user:{own:'u1', edits:['u2']}}
+  , {_id:'22222', file:{name:'test.mp4', path:'/u1/test.mp4', time:new Date(Date.now() + 20)}, share:'PRIVATE', user:{own:'u1'}}
+  , {_id:'33333', file:{name:'test.txt', path:'/u2/test.txt', time:new Date(Date.now() + 30)}, share:'PRIVATE', user:{own:'u2', views:['u1']}}
+  , {_id:'44444', file:{name:'test.xlsx', path:'/u2/test.xlsx', time:new Date(Date.now() + 40)}, share:'PRIVATE', user:{own:'u2'}}
+  , {_id:'55555', file:{name:'test.png', path:'/u1/test.png', time:new Date(Date.now() + 50)}, share:'PUBLIC', user:{own:'u1', edits:['u2']}}
 ];
 
 saver.save('file', '_id', files);
