34a70345eaa7201a3240ef90e7d94f485d3f251a
Sigel <entireboy@daum.net>
2013-07-25 17:22:18 +0900

    1. 파일 업로드 버튼 여러번 못 누르게 수정 2. 파일 업로드 시 spinner 뺑글뺑글

--- routes/common/common.js
+++ routes/common/common.js
@@ -5,6 +5,8 @@
 
 /**
  * 천단위 콤마(,) 등 숫자 관련 포멧을 변경해준다.
+ * opt가 주어지지 않거나 opt.unit이 지정된 값이 아닌 경우는 천단위 마다 콤마만 찍어서 반환한다.
+ *
  * @param {Json} [opt] 변경할 포멧 옵션 (주어지지 않는 경우 default로 천단위 마다 콤마만 찍어서 반환한다.)
  *   opt.unit {없음|'KMB'|'KMG'} 축약해서 보여줄 숫자 표현 형식 (소수점 1자리까지 보여준다.)
  *     없음: 천단위 마다 콤마만 추가된 숫자 표현
@@ -20,7 +22,7 @@ Number.prototype.numberFormat = function(opt) {
         var units = ['', 'K', 'M', 'B'];
         var cnt = 0;
         while(1000 <= result) {
-          result = Math.floor((result * 100) / 1000) / 100;
+          result = Math.round((result * 100) / 1000) / 100;
           cnt++;
         }
         return result + units[cnt];
@@ -29,7 +31,7 @@ Number.prototype.numberFormat = function(opt) {
         var units = ['B', 'K', 'M', 'G', 'T', 'P'];
         var cnt = 0;
         while(1024 <= result) {
-          result = Math.floor((result * 100) / 1024) / 100;
+          result = Math.round((result * 100) / 1024) / 100;
           cnt++;
         }
         return result + units[cnt];
